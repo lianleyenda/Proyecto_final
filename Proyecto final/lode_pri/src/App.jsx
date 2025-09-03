384caca62ab435f0bd4be7197063903817ea9d78
Lian Leyenda <leyenditas78@gmail.com>
2025-09-03 17:20:58 -0300

    termine las promociones me falta seguir con header

--- Proyecto final/lode_pri/src/App.jsx
+++ Proyecto final/lode_pri/src/App.jsx
@@ -1,7 +1,36 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <head>
+        <title>Lode_priburguer</title>
+      </head>
+
+      <body>
+        <div class="promo">
+          <h2 class="tipografia">
+            ¡Hoy tu hambre tiene premio! 🍔 Comprá 2 hamburguesas y la tercera
+            va de regalo —solo por tiempo limitado.
+          </h2>
+          <button>Ir</button>
+        </div>
+        <header>
+          <a class="division">Inicio</a>
+          <a class="division">Contactos</a>
+          <a class="division">Promociones</a>
+          <a class="division">Carrito</a>
+        </header>
+
+        <div>
+          <h2>este es el cominezo donde iria la foto</h2>
+        </div>
+        <div>
+          <h2>aca hirian los distintas hamburguesas</h2>
+        </div>
+      </body>
+    </>
+  );
 }
 
 export default App;
