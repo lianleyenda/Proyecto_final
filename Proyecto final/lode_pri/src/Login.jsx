306365321a57ab68c701a8c0c88f6dab96b4386d
Valentin Velazquez <valesergiovelazquez@gmail>
2025-09-10 16:54:45 -0300

    hice el inicio de sesion, registro y olvide contraseña. Tambien aprendi a usar el react router, creamos distintos css

--- Proyecto final/lode_pri/src/Login.jsx
+++ Proyecto final/lode_pri/src/Login.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import "./App.css";
+import "./login.css";
 
 export default function Login() {
   return (
--- Proyecto final/lode_pri/src/registro.jsx
+++ Proyecto final/lode_pri/src/registro.jsx
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./login.css";
 
 export default function Registro() {
   return (
